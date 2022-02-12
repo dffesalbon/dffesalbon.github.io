3d5ae37374a42d0dd5f0b536ca8d90030248ca75
dffesalbon <fesalbondaniel@gmail.com>
2022-02-12 20:28:18 +0800

    add site title

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import Content from "./components/content/Content";
 import AppFooter from "./components/footer/AppFooter";
 import AppHeader from "./components/header/AppHeader";
 import Main from "./components/main/Main";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "Daniel Fesalbon";
+  }, ["Daniel Fesalbon"]);
+
   return (
     <>
       <div className="App">
