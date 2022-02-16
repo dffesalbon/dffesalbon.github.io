7cc56c63f1910d9ca7cddc0a22ac42151c600f85
dffesalbon <fesalbondaniel@gmail.com>
2022-02-17 03:09:55 +0800

    update contact section

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,16 +1,41 @@
 import "./Contact.css";
-
+import { GoMarkGithub } from "react-icons/go";
+import { MdEmail } from "react-icons/md";
+import { AiFillLinkedin } from "react-icons/ai";
 const Contact = () => {
   return (
     <>
       <div className="card">
         <div className="row">
           <div className="column ">
-            <p className="title tile-column unselectable">Contact</p>
+            <p className="title tile-column unselectable">Contacts</p>
             <p></p>
           </div>
           <div className="column">
-            <p className="paragraph unselectable">
+            <div className="contact">
+              <div className="contact-icon">
+                <a href="https://github.com/dffesalbon">
+                  <GoMarkGithub />
+                </a>
+              </div>
+            </div>
+
+            <div className="contact">
+              <div className="contact-icon">
+                <a href="mailto:fesalbondaniel@gmail.com">
+                  <MdEmail />
+                </a>
+              </div>
+            </div>
+
+            <div className="contact">
+              <div className="contact-icon">
+                <a href="https://www.linkedin.com/in/daniel-fesalbon-a416261ba/">
+                  <AiFillLinkedin />
+                </a>
+              </div>
+            </div>
+            {/*<p className="paragraph unselectable">
               You can reach me out with the following:
             </p>
             <p className="paragraph unselectable">
@@ -27,7 +52,7 @@ const Contact = () => {
               <a href="https://www.linkedin.com/in/daniel-fesalbon-a416261ba/">
                 Daniel Fesalbon
               </a>
-            </p>
+            </p>*/}
           </div>
         </div>
       </div>
