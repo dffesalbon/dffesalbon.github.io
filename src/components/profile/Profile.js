e1497edb14aa0def1a79e226236f0437d023840b
dffesalbon <fesalbondaniel@gmail.com>
2022-05-13 23:37:40 +0800

    updates

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,16 +3,49 @@ import { IoSchoolOutline } from "react-icons/io5";
 import { CgWorkAlt } from "react-icons/cg";
 import { RiStackLine } from "react-icons/ri";
 import { MdContactPage } from "react-icons/md";
+import { GoMarkGithub } from "react-icons/go";
+import { MdEmail } from "react-icons/md";
+import { AiFillLinkedin } from "react-icons/ai";
+import { SiKaggle } from "react-icons/si";
 
 const Profile = () => {
   return (
     <>
       <div className="profile-pic"></div>
       <div className="profile-info unselectable">
-        <section>Hello world, I am Daniel Fesalbon</section>
-        <section>and I am working as a software engineer :)</section>
+        <section>
+          Hello world, I am Daniel Fesalbon and I am working as a software
+          engineer :)
+        </section>
+        <div className="info-prompt">
+          <div className="contact-icon">
+            <a href="https://www.kaggle.com/danielfesalbon">
+              <SiKaggle />
+            </a>
+          </div>
+        </div>
+        <div className="info-prompt">
+          <div className="contact-icon">
+            <a href="https://www.linkedin.com/in/daniel-fesalbon-a416261ba/">
+              <AiFillLinkedin />
+            </a>
+          </div>
+        </div>
+        <div className="info-prompt">
+          <div className="contact-icon">
+            <a href="https://github.com/dffesalbon">
+              <GoMarkGithub />
+            </a>
+          </div>
+        </div>
+        <div className="info-prompt">
+          <div className="contact-icon">
+            <a href="mailto:fesalbondaniel@gmail.com">
+              <MdEmail />
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="info-prompt unselectable">check my links below</div>
     </>
   );
 };
